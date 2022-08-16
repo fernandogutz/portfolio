5d8aaba410b9ee1a9c767e8d7f3e1e0364c3dc3a
fernandogutz <03fernandogutierrez@gmail.com>
2022-08-16 04:00:19 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/*
+Arreglar error para meterle JS al Menú Mobile
+
+document.getElementById('barMenu').addEventListener('click', function() {
+    document.getElementById('navMenu').addClass('activeNav');
+});
+
+document.getElementById('barMenu').addEventListener("click",function(){
+    console.log('Has hecho click');
+});
+
+const barMenu = document.getElementById('barMenu');
+console.log(barMenu); 
+
+if (barMenu) {
+  barMenu.addEventListener('click', () => {
+    console.log('btn clicked');
+  });
+}
+
+//Mejorar Menú
+document.getElementById("submit").onclick = function(event) {
+    alert("Submit button is clicked!");
+    event.preventDefault();
+}
+ */
